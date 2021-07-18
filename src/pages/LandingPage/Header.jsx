11d8b1c8ffd26bb5fc570a24b6806666335cb785
dexkode4 <dexkode4@gmail.com>
2021-07-18 14:29:50 +0100

    update

--- src/pages/LandingPage/Header.jsx
+++ src/pages/LandingPage/Header.jsx
@@ -9,7 +9,7 @@ import logo from '../../assets/images/logo.png';
 
 export const Header = () => {
     return (
-        <Flex justifyContent="space-between" px="24" py="5" position="absolute" top="0" left="0">
+        <Flex justifyContent="space-between" px="24" py="5" position="absolute" top="0" left="0" w="100%">
             <Box w="50px">
                 <Image src={logo} objectFit="cover" w="100%" />
             </Box>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {
   Box,
+  Flex
 } from '@chakra-ui/react';
 import { Header } from './Header';
 export const LandingPage = () => {
   return (
-    <Box textAlign="center" fontSize="xl" position="relative">
+    <Flex textAlign="center" fontSize="xl" position="relative">
       <Header/>
-    </Box>
+      <Box flex={0.75} borderWidth={1}>
+left
+      </Box>
+      <Box flex={0.25}>
+right
+      </Box>
+    </Flex>
   );
 };
